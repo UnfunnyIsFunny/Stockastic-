d1e80dfc7fd720f59997cbff3cb72eef29058ec3
Unfunnyisfunny <christian.dociu@gmail.com>
2025-06-20 14:46:18 +0200

    added train data

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from striprtf.striprtf import rtf_to_text
+import space
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
+    # print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+    # try:
+    #     with open('Training Data/Factiva-20250620-0646.rtf', 'r', encoding='utf-8') as f:
+    #         rtf_content = f.read()
+    #     plain_text = rtf_to_text(rtf_content)
+    #     with open('Articles_4.txt', 'w', encoding='utf-8') as f_out:
+    #         f_out.write(plain_text)
+    #         print("Conversion successful!")
+    # except Exception as e:
+    #     print(f"Error during conversion: {e}")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
